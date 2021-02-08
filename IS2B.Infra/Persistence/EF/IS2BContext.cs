76d30319b52f7b87661b91822a9c3867413c8a34
cleilson pereira <marcialwushu@hotmail.com>
2021-02-07 23:53:41 -0300

    :book: Persistence struture initi

---
+++ IS2B.Infra/Persistence/EF/IS2BContext.cs
@@ -0,0 +1,20 @@
+﻿using IS2B.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS2B.Infra.Persistence.EF
+{
+    class IS2BContext : DbContext
+    {
+        public DbSet<JsonLoginRequest> Login { get; set; }
+
+        public IS2BContext()
+        {
+
+        }
+
+
+    }
+}
---
+++ IS2B.Infra/Persistence/EF/Map/MapLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS2B.Infra.Persistence.EF.Map
+{
+    public class MapLogin
+    {
+
+    }
+}
